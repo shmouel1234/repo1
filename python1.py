69c8e28cf4cb7854616ecddba1f719c38cbfd432
shmouel1234 <80039625+shmouel1234@users.noreply.github.com>
2021-03-04 13:28:12 +0200

    Update python1.py

--- python1.py
+++ python1.py
@@ -2,3 +2,7 @@ name=input("hello! what is your name? ")
 print(f"welcome to git hub {name}")
 age=int(input("how old are you? "))
 print(f"so you were born in {2021-age}")
+for index, letter in enumerate(name):
+    print(f"letter {index+1}: {letter}")
+print(f"your name {len(name)} letters")
+  
